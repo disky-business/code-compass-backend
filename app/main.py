081425d0c7eaffe91cd6eccf6c2ad58dd96124d1
Afzal Rao <afrao@cisco.com>
2024-11-13 16:59:40 +0530

    HC2024: Refactored some files.

--- app/main.py
+++ app/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 from app.routes.routes import router
 from app.logger import logger
 
-app = FastAPI()
-logger.info("Starting FastAPI app...")
-app.include_router(router)
+def start_application():
+    app = FastAPI()
+    logger.info("Starting FastAPI server...")
+    app.include_router(router)
+
+if __name__=="__main__":
+    start_application()
