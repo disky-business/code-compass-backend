5f00390a8ef68447ef7b0d0f253887b0563920a9
Afzal Rao <afrao@cisco.com>
2024-11-13 15:23:41 +0530

    HC2024: Renamed field names.

--- app/service/code_churn_calculator_service.py
+++ app/service/code_churn_calculator_service.py
@@ -9,4 +9,4 @@ class CodeChurnCalculatorService:
     @classmethod
     def calculate_code_churn_for_file(self, file_path, github_client_service):
         commit_count = github_client_service.get_commit_count(file_path)
-        return (math.exp(-self.decay_factor * commit_count)) * 100
+        return round((math.exp(-self.decay_factor * commit_count)) * 100, 2)
